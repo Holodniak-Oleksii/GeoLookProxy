eea95f7e99c8c05573fbcd5a10e1f06f8cb93509
Oleksii Holodniak <arsak13.1881@gmail.com>
2025-08-12 19:46:06 +0300

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const axios = require("axios");
+const axios = require("axios");
 const cors = require("cors");
 const express = require("express");
 const app = express();
@@ -10,29 +10,29 @@ app.get("/", (req, res) => {
   res.send("Hello, I am proxy");
 });
 
-// app.get("/nominatim/reverse", async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       "https://nominatim.openstreetmap.org/reverse",
-//       { params: req.query }
-//     );
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Request failed" });
-//   }
-// });
+app.get("/nominatim/reverse", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/reverse",
+      { params: req.query }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Request failed" });
+  }
+});
 
-// app.get("/nominatim/details", async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       "https://nominatim.openstreetmap.org/details",
-//       { params: req.query }
-//     );
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Request failed" });
-//   }
-// });
+app.get("/nominatim/details", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/details",
+      { params: req.query }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Request failed" });
+  }
+});
 
 app.listen(port, () => console.log(`Local proxy running on port ${port}`));
 
