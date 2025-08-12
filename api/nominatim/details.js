00c86fa9d61d109bbae5e0107688ae1c2bdf7493
Oleksii Holodniak <arsak13.1881@gmail.com>
2025-08-12 19:56:45 +0300

    first commit

---
+++ api/nominatim/details.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+module.exports = async function handler(req, res) {
+  try {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/details",
+      {
+        params: req.query,
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Request failed" });
+  }
+};
