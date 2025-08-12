6cfd1dd6d98b03aedaa3302c8ac92ab2d493d54e
Oleksii Holodniak <arsak13.1881@gmail.com>
2025-08-12 18:43:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(cors());
+
+app.get("/nominatim/reverse", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/reverse",
+      {
+        params: req.query,
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Request failed" });
+  }
+});
+
+app.get("/nominatim/details", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/details",
+      {
+        params: req.query,
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Request failed" });
+  }
+});
+
+app.listen(3000, () => console.log("Proxy running on port 3000"));
