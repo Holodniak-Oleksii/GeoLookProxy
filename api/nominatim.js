56db0cba2957d354c17f6fed79cfdd424b18684e
Oleksii Holodniak <arsak13.1881@gmail.com>
2025-08-12 19:27:40 +0300

    first commit

--- api/nominatim.js
+++ api/nominatim.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-import cors from "cors";
-import express from "express";
+const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  res.send("Hello I am proxy");
+app.get("/", (req, res) => {
+  res.send("Hello, I am proxy");
 });
 
 app.get("/nominatim/reverse", async (req, res) => {
